8a365b8d5a9b7e86fc172e28c5d031897139caea
Pratik Kumar <pkumar@tavisca.com>
2017-09-25 09:13:41 +0530

    test project base

---
+++ ElasticSearchFixture/ElasticSearchFixture.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ElasticSearchFixture
+{
+    public class ElasticSearchFixture
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ RecordsManager/ElasticSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RecordsManager
+{
+    public class ElasticSearch
+    {
+        private string _ip = "172.16.14.171";
+        private int _port = 9200;
+
+        public ElasticSearch()
+        {
+
+        }
+    }
+}
