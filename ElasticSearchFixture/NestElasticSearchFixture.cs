7a8bedcf5443ba19179ce4502eb9ffb36641adda
Pratik Kumar <pkumar@tavisca.com>
2017-09-26 09:53:42 +0530

    nest connection initialization

---
+++ ElasticSearchFixture/NestElasticSearchFixture.cs
@@ -0,0 +1,15 @@
+﻿using RecordsManager;
+using Xunit;
+
+namespace ElasticSearchFixture
+{
+    public class NestElasticSearchFixture
+    {
+        [Fact]
+        public void CheckingInitialConnection()
+        {
+            NestElasticSearch nestElasticSearch = new NestElasticSearch();
+            nestElasticSearch.InitializeConnection();
+        }
+    }
+}
---
+++ RecordsManager/NestElasticSearch.cs
@@ -0,0 +1,27 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+
+namespace RecordsManager
+{
+    public class NestElasticSearch
+    {
+        private string[] _ips = { "172.16.14.171", "172.16.14.148", "172.16.14.90" };
+        private ElasticClient _client;
+
+        public void InitializeConnection()
+        {
+            string preUri = "http://";
+            Uri[] nodes = new Uri[_ips.Length];
+            for (int i = 0; i < _ips.Length; i++)
+            {
+                nodes[i] = new Uri(preUri + _ips[i]);
+            }
+            var pool = new StaticConnectionPool(nodes);
+            var settings = new ConnectionSettings(pool);
+            _client = new ElasticClient(settings);
+        }
+
+
+    }
+}
